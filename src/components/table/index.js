d1fa5e7dbe786e43ff4879ed00b51d40f5ed325e
subhanjee <subanjee14@gmail.com>
2025-04-12 13:29:53 +0500

    done

---
+++ src/components/table/index.js
@@ -0,0 +1,99 @@
+"use client";
+import React from "react";
+const data = [
+  {
+    email: "ai***1@gmail.com",
+    auctioned: "20.350",
+
+    time: "2 sec ago",
+  },
+  {
+    email: "ui***bc@gmail.com",
+    auctioned: "167,099.195",
+
+    time: "4 sec ago",
+  },
+  {
+    email: "rp***n@gmail.com",
+    auctioned: "158.761",
+
+    time: "5 min ago",
+  },
+  {
+    email: "dq***4@gmail.com",
+    auctioned: "54.995",
+
+    time: "10 min ago",
+  },
+  {
+    email: "bi***o@gmail.com",
+    auctioned: "594,830.365",
+
+    time: "20 min ago",
+  },
+  {
+    email: "mu***1@gmail.com",
+    auctioned: "20.000",
+
+    time: "30 min ago",
+  },
+  {
+    email: "rt***n@gmail.com",
+    auctioned: "20.000",
+
+    time: "50 min ago",
+  },
+  {
+    email: "kl***o@gmail.com",
+    auctioned: "594,830.365",
+
+    time: "1 hour ago",
+  },
+  {
+    email: "qw***e@outlook.com",
+    auctioned: "81.264",
+
+    time: "2 hour ago",
+  },
+];
+
+export default function InvestingTable() {
+  return (
+    <div className="overflow-x-auto p-4 text-white bg-[#262b2d] border border-gray-600 rounded-2xl shadow-2xl ">
+      <table className="min-w-[20rem] md:min-w-[20rem] lg:min-w-[20rem] xl:min-w-[40rem]   ">
+        <thead className="  text-sm">
+          <tr>
+            <th className="px-4 py-3 text-left border-b border-gray-600">#</th>
+            <th className="px-4 py-3 text-left border-b border-gray-600">
+              Investing Users
+            </th>
+            <th className="px-4 py-3 text-left border-b border-gray-600">
+              USDT Deposited{" "}
+            </th>
+            <th className="px-4 py-3 text-left border-b border-gray-600">
+              Time
+            </th>
+          </tr>
+        </thead>
+        <tbody className="text-sm">
+          {data.map((item, idx) => (
+            <tr key={idx} className="hover:bg-gray-700 transition duration-150">
+              <td className="px-4 py-3 border-b border-gray-700 text-gray-400">
+                {idx + 1}
+              </td>
+              <td className="px-4 py-3 border-b border-gray-700">
+                {item.email}
+              </td>
+              <td className="px-4 py-3 text-green-400 border-b border-gray-700">
+                {item.auctioned}
+              </td>
+              <td className="px-4 py-3 text-gray-400 border-b border-gray-700">
+                {item.time}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
--- src/pages/landingPage/index.js
+++ src/pages/landingPage/index.js
@@ -1,29 +1,32 @@
-import React, { useState } from 'react'
-import Faqs from '@/src/components/faqs'
-import FeaturesSection from '@/src/components/feature'
-import Footer from '@/src/components/footer'
-import Header from '@/src/components/header'
-import HeroSection from '@/src/components/heroSection'
-import Roadmap from '@/src/components/roadMap'
-import Unlocking from '@/src/components/unlocking'
-import USDTDeposit from '@/src/components/USDTDeposit'
-import AuthPageWrapper from '@/pages/AuthPageWrapper'
+import React, { useState } from "react";
+import Faqs from "@/src/components/faqs";
+import FeaturesSection from "@/src/components/feature";
+import Footer from "@/src/components/footer";
+import Header from "@/src/components/header";
+import HeroSection from "@/src/components/heroSection";
+import Roadmap from "@/src/components/roadMap";
+import Unlocking from "@/src/components/unlocking";
+import USDTDeposit from "@/src/components/USDTDeposit";
+import AuthPageWrapper from "@/pages/AuthPageWrapper";
+import InvestingTable from "@/src/components/table";
 
 function LandingPage() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
-  const [showAuthModal, setShowAuthModal] = useState(false)
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [showAuthModal, setShowAuthModal] = useState(false);
 
   return (
     <div>
       <Header onJoinClick={() => setShowAuthModal(true)} />
-        <div className='flex flex-col items-center justify-center min-h-screen text-white'
-          style={{
-            backgroundImage:
-              "url('https://satochain.io/images/home/build-for-bitcoin.svg')",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-          }}>
+      <div
+        className="flex flex-col items-center justify-center min-h-screen text-white"
+        style={{
+          backgroundImage:
+            "url('https://satochain.io/images/home/build-for-bitcoin.svg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
         <h1 className="text-3xl text-center font-mono mt-16 md:text-5xl lg:text-8xl">
           Build for Bitcoin
         </h1>
@@ -33,15 +36,13 @@ function LandingPage() {
         <p className="text-1xl md:text-2xl lg:3xl">
           Bitcoin-based apps and smart contracts
         </p>
-      <div
-  className='min-h-screen grid grid-cols-1  gap-5 md:grid-cols-1 lg:grid-cols-2 text-white'
-
->
-  
+        <div className="min-h-screen w-full flex flex-col lg:flex-row items-center justify-center gap-8 px-12 py-12 text-white">
   <HeroSection />
-  {isAuthenticated && <USDTDeposit />}
+  {!isAuthenticated ? <InvestingTable /> : <USDTDeposit />}
 </div>
-</div>
+
+
+      </div>
       <FeaturesSection />
       <Unlocking />
       <Roadmap />
@@ -58,15 +59,17 @@ function LandingPage() {
             >
               &times;
             </button>
-            <AuthPageWrapper onAuthSuccess={() => {
-              setIsAuthenticated(true)
-              setShowAuthModal(false)
-            }} />
+            <AuthPageWrapper
+              onAuthSuccess={() => {
+                setIsAuthenticated(true);
+                setShowAuthModal(false);
+              }}
+            />
           </div>
         </div>
       )}
     </div>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
